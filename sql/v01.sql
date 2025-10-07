c752e5a9dc6eaba418df41968b6614215b52e9e5
marcosvitordev <marcosvitorlima531@gmail.com>
2025-10-06 23:40:28 -0300

    banco de dados

--- sql/v01.sql
+++ sql/v01.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `sistema_saude_municipal` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `sistema_saude_municipal`;
 -- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sistema_saude_municipal
@@ -539,4 +537,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-06 23:33:36
+-- Dump completed on 2025-10-06 23:39:53
