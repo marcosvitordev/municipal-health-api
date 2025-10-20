c62df64870faf1843a522276c2ec40693271dfab
marcosvitordev <marcosvitorlima531@gmail.com>
2025-10-19 22:26:42 -0300

    API 1.0

--- src/routes/statsRoutes.js
+++ src/routes/statsRoutes.js
@@ -5,7 +5,9 @@ const role = require('../middlewares/rbacMiddleware');
 const ctl = require('../controllers/statsController');
 
 // Rota para estatísticas dos municípios
-r.get('/stats/municipalities', a, role(['admin']), ctl.municipalityStats);
+// r.get('/stats/municipalities', a, role(['admin']), ctl.municipalityStats);
+// Em src/routes/statsRoutes.js
+r.get('/stats/municipalities', a, role(['admin','institute']), ctl.municipalityStats);
 
 // Rota para logs de auditoria
 r.get('/audit-logs', a, role(['admin']), ctl.auditLogs);
